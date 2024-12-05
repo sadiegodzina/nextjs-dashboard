6eb7fa3680a7c320c9ce8b3372066d323986e18c
sadiegodzina <sadiegodzina@gmail.com>
2024-12-04 19:11:41 -0500

    initial commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,13 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
 
-const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "MO-PCCO.R3 - NextJS Financial Dashboard",
-  description: "Starter code for Module 25 Capstone project - Financial Dashbard",
-};
-
-export default function RootLayout({ children }) {
+export default function RootLayout({ 
+  children,
+ }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
